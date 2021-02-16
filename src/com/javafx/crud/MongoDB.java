7520e7e010c260cb4bf087c31846759c301e5c64
Alex Rodkin <alexnikrod@users.noreply.github.com>
2021-02-16 19:02:56 +0100

    MongoDB URI for cloud server added

--- src/com/javafx/crud/MongoDB.java
+++ src/com/javafx/crud/MongoDB.java
@@ -1,29 +1,43 @@
 package com.javafx.crud;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class MongoDB {
+    public final static String URI_SRV = "mongodb+srv://admin:admin@cluster0.vx6ge.mongodb.net/Database?retryWrites=true&w=majority";
     public final static String HOST = "localhost";
     public final static int PORT = 27017;
 
+    public static MongoClientURI mongoUri;
     public static MongoClient mongoClient;
     public static MongoDatabase mongoDatabase;
     public static MongoCollection<Document> mongoCollection;
     public static MongoCursor<Document> mongoCursor;
 
     public static void connection() {
-        // create a connection to mongodb server
+        // create a connection to mongodb local server
         mongoClient = new MongoClient(HOST, PORT);
+
+        // create a connection to mongodb cloud server
+        // mongoUri = new MongoClientURI(URI_SRV);
+        // mongoClient = new MongoClient(mongoUri);
 
         // create a database name
         mongoDatabase = mongoClient.getDatabase("Database");
 
         // create a collection
         mongoCollection = mongoDatabase.getCollection("Persons");
+
+
+
+
+
+
+
     }
 
     public static MongoCursor<Document> findAllItems() {
